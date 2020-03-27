212b65f366e466dda0565e4ba0119441d51d20c6
Mariiacristina <mariacristina.binfa@mail.udp.cl>
2020-03-27 19:20:35 -0300

    Updates

---
+++ precache-manifest.9135b92cb3343e4bc26ea4301af12ec3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "819140c1b0d33e0cd1a6f88bea4db692",
+    "url": "/test1/index.html"
+  },
+  {
+    "revision": "557ebbf915742acaf6eb",
+    "url": "/test1/static/css/main.3e747f80.chunk.css"
+  },
+  {
+    "revision": "6985b7fec760746ffc94",
+    "url": "/test1/static/js/2.c5c5fc67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test1/static/js/2.c5c5fc67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "557ebbf915742acaf6eb",
+    "url": "/test1/static/js/main.b3e8385f.chunk.js"
+  },
+  {
+    "revision": "d8b9c66b465b03051477",
+    "url": "/test1/static/js/runtime-main.e073c473.js"
+  }
+]);
